bf05d40638eb9634df9f96f2177c064f17c8ab22
shell7 <shellqiqi@outlook.com>
2018-05-20 21:32:12 +0800

    Init project

---
+++ src/main/kotlin/seu/App.kt
@@ -0,0 +1,5 @@
+package seu
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
---
+++ src/test/kotlin/seu/AppTest.kt
@@ -0,0 +1,8 @@
+package seu
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class AppTest {
+
+}
