d4327fb15b864b5a02e1cbb24d50b8c77150609a
shell7 <shellqiqi@outlook.com>
2018-05-28 11:14:40 +0800

    add functions in YaccFile left readRules

--- src/main/kotlin/seu/io/YaccFile.kt
+++ src/main/kotlin/seu/io/YaccFile.kt
@@ -8,8 +8,55 @@ class YaccFile(filePath: String) {
     private var reader: BufferedReader = BufferedReader(FileReader(filePath))
 
     var headers: StringBuffer = StringBuffer()
-    var instructions: HashMap<String, HashSet<String>> = HashMap()
+    var instructions: HashMap<String, String> = HashMap()
     var rules: HashMap<String, ArrayList<String>> = HashMap()
     var userSeg: StringBuffer = StringBuffer()
 
+    init {
+        reader = BufferedReader(FileReader(filePath))
+        readInstructions()
+        readRules()
+        readUserSeg()
+    }
+
+    private fun readInstructions() {
+        while (true) {
+            val lineOfReader = reader.readLine() ?: throw Exception("Lex format error - miss macro definitions")
+            when {
+                lineOfReader.startsWith("%%") -> return
+                lineOfReader.startsWith("%{") -> readHeaders()
+                else -> {
+                    val split = lineOfReader.split(' ')
+                    val tag = split[0]
+                    val token = split[1]
+                    instructions[token] = tag
+                }
+            }
+        }
+    }
+
+    private fun readHeaders() {
+        while (true) {
+            val lineOfReader = reader.readLine() ?: throw Exception("Lex format error - miss another \"%}\"")
+            if (lineOfReader.startsWith("%}")) return
+            else headers.append(lineOfReader).append("\n")
+        }
+    }
+
+    private fun readRules() {
+        while (true) {
+            val lineOfReader = reader.readLine() ?: throw Exception("Lex format error - miss user segment")
+            if (lineOfReader.startsWith("%%")) return
+            else {
+                // TODO: parse rules
+            }
+        }
+    }
+
+    private fun readUserSeg() {
+        while (true) {
+            val lineOfReader = reader.readLine() ?: return
+            userSeg.append(lineOfReader).append("\n")
+        }
+    }
 }
--- src/test/kotlin/seu/AppTest.kt
+++ src/test/kotlin/seu/AppTest.kt
@@ -1,8 +1,45 @@
 package seu
 
+import org.junit.BeforeClass
 import org.junit.Test
+import seu.io.YaccFile
 import kotlin.test.assertEquals
 
 class AppTest {
 
+    companion object {
+        private lateinit var yaccFile: YaccFile
+        @BeforeClass @JvmStatic fun constructor() {
+            yaccFile = YaccFile("resource/example.y")
+        }
+    }
+
+    @Test fun readHeaders() {
+        assertEquals("#include <ctype.h>\n", yaccFile.headers.toString())
+    }
+
+    @Test fun readInstructions() {
+        val instructions: HashMap<String, String> = HashMap()
+        instructions["DIGIT"] = "%token"
+        assertEquals(instructions, yaccFile.instructions)
+    }
+
+    @Test fun readRules() {
+        // TODO
+    }
+
+    @Test fun readUserSeg() {
+        assertEquals("""
+        yylex() {
+            int c;
+            c = getchar();
+            if (isdigit(c)) {
+                yylval = c-'0';
+                return DIGIT;
+            }
+            return c;
+        }
+
+        """.trimIndent(), yaccFile.userSeg.toString())
+    }
 }
