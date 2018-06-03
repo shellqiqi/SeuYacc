16ab3ee2ced3ae441d1c66e352058c3b40dd3070
XUCHENGZHUO <912342675@qq.com>
2018-06-03 17:36:39 +0800

    correct mistakes in function main in APP

--- src/main/kotlin/seu/App.kt
+++ src/main/kotlin/seu/App.kt
@@ -6,8 +6,5 @@ import seu.lr.Production
 
 fun main(args: Array<String>) {
     val yaccFile: YaccFile = YaccFile("resource/example.y")
-    val termi : HashSet<String> = HashSet()
-    val non_termi : HashSet<String> = HashSet()
-    yaccFile.rules.forEach{(t,u)->termi.add(t.left)}
-    var lr: LR = LR(yaccFile.rules.keys.toList(), termi,non_termi , "" )
+    var lr: LR = LR(yaccFile.rules.keys.toList(), yaccFile.terminals, yaccFile.non_terminals, "")
 }
