394c12a666031df73d3cd045622c0bf00f58a067
shell7 <shellqiqi@outlook.com>
2018-06-04 20:26:57 +0800

    complete parsing table

---
+++ src/test/kotlin/seu/lr/ParsingTableTest.kt
@@ -0,0 +1,50 @@
+package seu.lr
+
+import org.junit.Test
+
+import org.junit.BeforeClass
+import seu.io.YaccFile
+import kotlin.test.assertEquals
+
+class ParsingTableTest {
+
+    companion object {
+        private lateinit var state1: State
+        private lateinit var state2: State
+        private lateinit var production: Production
+        private var table = ParsingTable()
+        @BeforeClass
+        @JvmStatic
+        fun constructor() {
+            val yaccFile1 = YaccFile("resource/example.y")
+            val lr = LR(yaccFile1.rules.keys.toList(), "")
+            val item1 = Item(lr.productions[5], 0, Symbol.END)
+            val item2 = Item(lr.productions[4], 1, Symbol.END)
+            state1 = lr.closure(arrayListOf(item1))
+            state2 = lr.closure(arrayListOf(item2))
+            production = lr.productions[0]
+        }
+    }
+
+    @Test
+    fun getAndSetTest() {
+        table[state1, Symbol(Symbol.NON_TERMINAL, "E")] =
+                ParsingTable.Entry(ParsingTable.Entry.SHIFT, state2)
+        table[state1, Symbol(Symbol.TERMINAL, "F")] =
+                ParsingTable.Entry(ParsingTable.Entry.ACCEPT, null)
+        table[state2, Symbol(Symbol.NON_TERMINAL, "G")] =
+                ParsingTable.Entry(ParsingTable.Entry.REDUCE, production)
+        assertEquals(
+                ParsingTable.Entry(ParsingTable.Entry.SHIFT, state2),
+                table[state1, Symbol(Symbol.NON_TERMINAL, "E")])
+        assertEquals(
+                ParsingTable.Entry(ParsingTable.Entry.ACCEPT, null),
+                table[state1, Symbol(Symbol.TERMINAL, "F")])
+        assertEquals(
+                ParsingTable.Entry(ParsingTable.Entry.REDUCE, production),
+                table[state2, Symbol(Symbol.NON_TERMINAL, "G")])
+        assertEquals(
+                null,
+                table[state1, Symbol(Symbol.TERMINAL, "E")])
+    }
+}
