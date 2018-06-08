19dc3ede481a81be0954a9ae65d27497863d8815
shell7 <shellqiqi@outlook.com>
2018-06-08 23:42:04 +0800

    delete CodeFileTest and transform CodeFile from fake kotlin to real kotlin

--- src/main/kotlin/seu/io/CodeFile.kt
+++ src/main/kotlin/seu/io/CodeFile.kt
@@ -4,13 +4,11 @@ import seu.lr.LR
 
 import java.io.BufferedWriter
 import java.io.FileWriter
-import java.io.IOException
 
 class CodeFile(private val yaccFile: YaccFile, private val lr: LR) {
 
     private lateinit var writer: BufferedWriter
 
-    @Throws(IOException::class)
     fun writeFile(filePath: String) {
         writer = BufferedWriter(FileWriter(filePath))
         writeHeaders()
@@ -19,14 +17,11 @@ class CodeFile(private val yaccFile: YaccFile, private val lr: LR) {
         writer.close()
     }
 
-    @Throws(IOException::class)
-    fun writeHeaders() {
+    private fun writeHeaders() {
         writer.write(yaccFile.headers.toString())
     }
 
-    @Throws(IOException::class)
-    fun writeUserSeg() {
+    private fun writeUserSeg() {
         writer.write(yaccFile.userSeg.toString())
     }
-
 }
