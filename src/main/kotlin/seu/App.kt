be1f6ec77b65ef23c1b391dc61c8c014baa286f2
shell7 <shellqiqi@outlook.com>
2018-06-10 16:39:01 +0800

    complete app

--- src/main/kotlin/seu/App.kt
+++ src/main/kotlin/seu/App.kt
@@ -1,9 +1,16 @@
 package seu
 
+import seu.io.CodeFile
 import seu.io.YaccFile
 import seu.lr.LR
 
 fun main(args: Array<String>) {
-    val yaccFile = YaccFile("resource/example.y")
-//    var lr = LR(yaccFile.rules.keys.toList(), yaccFile.terminals, yaccFile.nonTerminals, "")
+    if (args.size != 2) {
+        println("Please input a file.")
+        return
+    }
+    val yaccFile = YaccFile(args[0])
+    val lr = LR(yaccFile.rules.toList(), yaccFile.start)
+    val codeFile = CodeFile(yaccFile, lr)
+    codeFile.writeFile(args[1])
 }
